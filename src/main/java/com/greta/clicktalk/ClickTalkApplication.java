fb6d886ca4ed395da28b3511fadd2797efa7efb2
Ayman ALKHATIB <aemn22384@gmail.com>
2025-02-17 13:52:44 +0100

    Add initial project structure with Maven configuration and application setup

---
+++ src/main/java/com/greta/clicktalk/ClickTalkApplication.java
@@ -0,0 +1,13 @@
+package com.greta.clicktalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greta/clicktalk/ClickTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greta.clicktalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
