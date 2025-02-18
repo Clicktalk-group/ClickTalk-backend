c632482d1d878bd128ec6c3a68462ade002a0bb9
Ayman ALKHATIB <aemn22384@gmail.com>
2025-02-18 16:12:33 +0100

    Add PasswordUpdateService for handling password updates

---
+++ src/main/java/com/greta/clicktalk/serveces/PasswordUpdateService.java
@@ -0,0 +1,31 @@
+package com.greta.clicktalk.serveces;
+
+import com.greta.clicktalk.DAOs.UserDao;
+import com.greta.clicktalk.DTOs.UpdatePasswordRequestDTO;
+import com.greta.clicktalk.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordUpdateService {
+    private final UserDao userDao;
+    private final PasswordEncoder encoder;
+
+    public PasswordUpdateService(UserDao userDao, PasswordEncoder encoder) {
+        this.userDao = userDao;
+        this.encoder = encoder;
+    }
+
+    public ResponseEntity<String> updatePassword( UpdatePasswordRequestDTO updatePasswordRequestDTO, Authentication authentication) {
+        User currentUser = userDao.findByEmail(authentication.getName());
+
+        boolean passwordMatches = encoder.matches(updatePasswordRequestDTO.getCurrentPassword(), currentUser.getPassword());
+
+        if(passwordMatches) {
+            return userDao.updatePassword(currentUser.getEmail(), encoder.encode(updatePasswordRequestDTO.getNewPassword()));
+        }
+        return ResponseEntity.badRequest().body("Error: your password is not correct !");
+    }
+}
