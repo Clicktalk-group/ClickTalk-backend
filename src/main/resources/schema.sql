8e8d682897f83f9b92fe122158e38a8a8c308245
Ayman ALKHATIB <aemn22384@gmail.com>
2025-02-18 10:21:44 +0100

    Normalize table names to lowercase and add role column to Users table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,34 +2,35 @@ CREATE DATABASE IF NOT EXISTS clicktalk_db;
 
 use clicktalk_db;
 
-CREATE TABLE IF NOT EXISTS Users (
+CREATE TABLE IF NOT EXISTS users (
         id BIGINT AUTO_INCREMENT PRIMARY KEY ,
         email VARCHAR(255) NOT NULL UNIQUE,
         password VARCHAR(255) NOT NULL,
+        role VARCHAR(50) NOT NULL,
         created_at TIMESTAMP DEFAULT NOW()
 );
 
-CREATE TABLE IF NOT EXISTS Conversations (
+CREATE TABLE IF NOT EXISTS conversations (
         id BIGINT AUTO_INCREMENT PRIMARY KEY,
         user_id bigint NOT NULL,
         title VARCHAR(255) NOT NULL,
         created_at TIMESTAMP DEFAULT NOW(),
-        FOREIGN KEY (user_id) REFERENCES Users(id)
+        FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE IF NOT EXISTS Messages (
+CREATE TABLE IF NOT EXISTS messages (
         id BIGINT AUTO_INCREMENT PRIMARY KEY,
         conv_id BIGINT NOT NULL,
         content LONGTEXT NOT NULL,
         is_bot BOOLEAN NOT NULL,
         created_at TIMESTAMP DEFAULT NOW(),
-        FOREIGN KEY (conv_id) REFERENCES Conversations(id)
+        FOREIGN KEY (conv_id) REFERENCES conversations(id)
 );
 
-CREATE TABLE IF NOT EXISTS Settings (
+CREATE TABLE IF NOT EXISTS settings (
         id BIGINT AUTO_INCREMENT PRIMARY KEY,
         user_id BIGINT NOT NULL,
         theme VARCHAR(255) NOT NULL,
-        FOREIGN KEY (user_id) REFERENCES Users(id)
+        FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
